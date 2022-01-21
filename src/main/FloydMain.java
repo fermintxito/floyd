eab48fd05da9571650f50ba79e90b73120455e91
Fermin Gorraiz <fermin.gorraiz@gmail.com>
2022-01-21 13:36:35 +0100

    First commit

---
+++ src/main/FloydMain.java
@@ -0,0 +1,51 @@
+package main;
+
+import main.common.CommonApiDate;
+import main.common.CommonApiMenu;
+
+public class FloydMain {
+
+	private static final String DEFAULT_OUTPUT = "pruebas/output/defaultOutput.txt";
+
+	private static final String SHOW_TRACE = "-t";
+	private static final String SHOW_HELP = "-h";
+
+	private static boolean showTrace = false;
+	
+	public static void main(String[] args) {
+		System.out.println(main.common.CommonApiDate.dateNowToString() + " || Launch Skyline\r\n");
+		boolean isInput = true;
+
+		String inputFile = null;
+		String outputFile = DEFAULT_OUTPUT;
+
+		if (args.length > 4) {
+			System.out.println("Too much args, see [skyline -h] for help\r\n");
+			return;
+		} else if (args.length < 1) {
+			System.out.println("Not enough arguments, see [skyline -h] for help\r\n");
+			return;
+		}
+
+		for (String a : args) {
+			switch (a) {
+			case SHOW_TRACE:
+				showTrace = true;
+				continue;
+			case SHOW_HELP:
+				System.out.println(CommonApiMenu.showHelp());
+				continue;
+			}
+
+			if (isInput) {
+				inputFile = a;
+				isInput = false;
+			} else {
+				outputFile = a;
+			}
+		}
+
+		Floyd.run(inputFile, outputFile, showTrace);
+		System.out.println(CommonApiDate.dateNowToString() + " || Close Skyline\r\n");
+	}
+}
